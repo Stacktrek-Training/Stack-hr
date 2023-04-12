1dd5e856a39228a5cac8b0233e63ba3a99ea7f35
kennedy <sunny.virgo@antiquespride.edu.ph>
2023-04-12 15:23:30 +0800

    first commit

--- server/index.js
+++ server/index.js
@@ -6,6 +6,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/employee", async (req, res) =>{
+    try {
+        const getEmployee = await pool.query("SELECT * FROM employee")
+        res.json(getEmployee.rows)
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.get("/employee/:id", async (req, res) =>{
+    try {
+        const { id }= req.params;
+        const getEMP = await pool.query("SELECT * FROM employee WHERE employee_id=$1", [id])
+        res.json(getEMP.rows)
+    } catch (error) {
+        console.error(error.message)
+    }
+})
 
 app.listen(4000, () =>{
     console.log('Listening to port 4000');
