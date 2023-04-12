5b20dacecd2a0d3f19bdfd1340494bc199f9435e
SunnyVirgo <sunny.virgo@antiquespride.edu.ph>
2023-04-12 19:33:40 +0800

    "first commit"

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+//for employee
 
+//get all employees
 app.get("/employee", async (req, res) =>{
     try {
         const getEmployee = await pool.query("SELECT * FROM employee")
@@ -14,7 +16,7 @@ app.get("/employee", async (req, res) =>{
         console.error(error.message)
     }
 })
-
+//get employee by id
 app.get("/employee/:id", async (req, res) =>{
     try {
         const { id }= req.params;
@@ -24,7 +26,30 @@ app.get("/employee/:id", async (req, res) =>{
         console.error(error.message)
     }
 })
+// add employee
+app.post("/employee", async (req, res) =>{
+    try {
+        const { full_name, job_title } =req.body;
+        const insertEmployee = await pool.query("INSERT INTO employee (full_name, job_title)VALUES($1, $2) RETURNING *", [full_name, job_title])
+        res.json("Inserted data")
+        
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+// edit employee
+app.put("/employee/:id", async (req, res)=>{
+    try {
+        const { id } = req.params
+        const { full_name, job_title } = req.body
+        const updateEmp = await pool.query("UPDATE employee SET full_name=$1, job_title=$2 WHERE employee_id =$3", [full_name, job_title, id])
+        res.json(updateEmp.rows)
+    } catch (error) {
+        console.error(error.message)
+    }
+})
 
+//end of employee
 app.listen(4000, () =>{
     console.log('Listening to port 4000');
 });
