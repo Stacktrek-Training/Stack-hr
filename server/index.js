8d33b17689f1329b117c2eb83d1f073f2b8d0a92
SunnyVirgo <sunny.virgo@antiquespride.edu.ph>
2023-04-18 11:01:26 +0800

    update CRUD for employee

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const pool = require('./database');
+const express = require("express");
+const cors = require("cors");
+const pool = require("./database");
 const app = express();
 
 app.use(cors());
@@ -8,57 +8,154 @@ app.use(express.json());
 //for employee
 
 //get all employees
-app.get("/employee", async (req, res) =>{
-    try {
-        const getEmployee = await pool.query("SELECT * FROM EMPLOYEES")
-        res.json(getEmployee.rows)
-    } catch (error) {
-        console.error(error.message)
-    }
-})
+app.get("/employee", async (req, res) => {
+  try {
+    const getEmployee = await pool.query(`SELECT * FROM "EMPLOYEES"`);
+    res.json(getEmployee.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 //get employee by id
-app.get("/employee/:id", async (req, res) =>{
-    try {
-        const { id }= req.params;
-        const getEMP = await pool.query("SELECT * FROM EMPLOYEES WHERE employee_id=$1", [id])
-        res.json(getEMP.rows)
-    } catch (error) {
-        console.error(error.message)
-    }
-})
+app.get("/employee/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getEMP = await pool.query(
+      `SELECT * FROM "EMPLOYEES" WHERE employee_id=$1`,
+      [id]
+    );
+    res.json(getEMP.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 // add employee
-app.post("/employee", async (req, res) =>{
-    try {
-        const { firstname, middlename, lastname, address, contact, date_inserted, date_updated, job_title } =req.body;
-        const insertEmployee = await pool.query("INSERT INTO EMPLOYEES (firstname, middlename, lastname, address, contact, date_inserted, date_updated, job_title)VALUES($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, $6, $7) RETURNING *", [firstname, middlename, lastname, address, contact, date_updated, job_title])
-        res.json("Inserted data")
-        
-    } catch (error) {
-        console.error(error.message)
-    }
-})
+app.post("/employee", async (req, res) => {
+  try {
+    const {
+      first_name,
+      middle_name,
+      last_name,
+      province,
+      city,
+      municipality,
+      baranggay,
+      zipcode,
+      mobile_number,
+      telephone_number,
+      work_email,
+      personal_email,
+      emergency_contact_person,
+      emergency_contact_email,
+      emergency_contact_number,
+      relationship,
+      job_title,
+    } = req.body;
+    const insertEmployee = await pool.query(
+      `INSERT INTO "EMPLOYEES"(first_name,middle_name,last_name,province,city,municipality,baranggay,zipcode,mobile_number,telephone_number,work_email,personal_email,emergency_contact_person,emergency_contact_email,emergency_contact_number,relationship,job_title,date_created,status)VALUES($1, $2, $3, $4, $5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17, CURRENT_TIMESTAMP,0) RETURNING *`,
+      [
+        first_name,
+        middle_name,
+        last_name,
+        province,
+        city,
+        municipality,
+        baranggay,
+        zipcode,
+        mobile_number,
+        telephone_number,
+        work_email,
+        personal_email,
+        emergency_contact_person,
+        emergency_contact_email,
+        emergency_contact_number,
+        relationship,
+        job_title,
+      ]
+    );
+    res.json("Inserted data");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 // edit employee
-app.put("/employee/:id", async (req, res)=>{
-    try {
-        const { id } = req.params
-        const { firstname, middlename, lastname, address, contact, date_inserted, date_updated, job_title  } = req.body
-        const updateEmp = await pool.query("UPDATE EMPLOYEES SET firstname=$1, middlename=$2, lastname=$3, address=$4, contact=$5, date_inserted=$6, date_updated=CURRENT_TIMESTAMP, job_title=$7  WHERE employee_id =$8", [firstname, middlename, lastname, address, contact, date_inserted, job_title, id])
-        res.json(updateEmp.rows)
-    } catch (error) {
-        console.error(error.message)
-    }
-})
+app.put("/employee/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      first_name,
+      middle_name,
+      last_name,
+      province,
+      city,
+      municipality,
+      baranggay,
+      zipcode,
+      mobile_number,
+      telephone_number,
+      work_email,
+      personal_email,
+      emergency_contact_person,
+      emergency_contact_email,
+      emergency_contact_number,
+      relationship,
+      job_title,
+    } = req.body;
+    const updateEmp = await pool.query(
+      `UPDATE "EMPLOYEES" SET first_name=$1,middle_name=$2,last_name=$3,province=$4,city=$5,municipality=$6,baranggay=$7,zipcode=$8,mobile_number=$9,telephone_number=$10,work_email=$11,personal_email=$12,emergency_contact_person=$13,emergency_contact_email=$14,emergency_contact_number=$15,relationship=$16,job_title=$17,date_updated=CURRENT_TIMESTAMP  WHERE employee_id =$18`,
+      [
+        first_name,
+        middle_name,
+        last_name,
+        province,
+        city,
+        municipality,
+        baranggay,
+        zipcode,
+        mobile_number,
+        telephone_number,
+        work_email,
+        personal_email,
+        emergency_contact_person,
+        emergency_contact_email,
+        emergency_contact_number,
+        relationship,
+        job_title,
+        id,
+      ]
+    );
+    res.json(updateEmp.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 //delete employee
-app.delete("/employee/:id", async (req, res) =>{
-    try {
-        const { id } = req.params
-        const deleteEmp = await pool.query("DELETE FROM EMPLOYEES WHERE employee_id = $1 ", [id])
-        res.json("Employee deleted")
-    } catch (error) {
-        console.error(error.message)
-    }
-})
+app.delete("/employee/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteEmp = await pool.query(
+      "DELETE FROM EMPLOYEES WHERE employee_id = $1 ",
+      [id]
+    );
+    res.json("Employee deleted");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 //end of employee
-app.listen(4000, () =>{
-    console.log('Listening to port 4000');
+
+//salaries
+
+app.post("/salaries/", async (req, res) => {
+  try {
+    const insertSalary =
+      ("INSERT INTO SALARIES (employee_id, salary, status, date_created, date_updated) VALUES($1, $2, 0, CURRENT TIMESTAMP)",
+      [employee_id, salary]);
+    res.json(insertSalary.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+app.listen(4000, () => {
+  console.log("Listening to port 4000");
 });
