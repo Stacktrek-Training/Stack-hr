0372e1bc6b8c09e2fd9381bf8c2b7dc3e7449135
16HarryBing <Harrybing.raba@antiquespride.edu.ph>
2023-04-19 15:47:44 +0800

    Commit

--- client/src/components/status_salaries.jsx
+++ client/src/components/status_salaries.jsx
@@ -10,7 +10,7 @@ const StatusSalaries = () => {
   return (
     <>
       <span
-        className={`bg-blue-800 text-white font-bold py-1 px-2 text-md rounded cursor-pointer ${
+        className={`bg-blue-800 text-white font-bold py-1 px-2 hover:bg-blue-700 text-md rounded cursor-pointer ${
           isEnable ? "bg-blue-800" : "bg-red-500 !important"
         }`}
         onClick={handleClick}
