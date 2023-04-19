aeeaad7a3dab838369876b15d261e6d142e72df7
16HarryBing <Harrybing.raba@antiquespride.edu.ph>
2023-04-19 11:10:58 +0800

    Status Update

--- client/src/components/status_salaries.jsx
+++ client/src/components/status_salaries.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const StatusSalaries = () => {
+  const [isEnable, setIsActive] = useState(true);
+
+  const handleClick = () => {
+    setIsActive(!isEnable);
+  };
+
   return (
     <>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded active:bg-blue-800">
-        Active
-      </button>
-      <button class="bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded">
-        Inactive
-      </button>
+      <span
+        className={`bg-blue-800 text-white font-bold py-1 px-2 text-md rounded cursor-pointer ${
+          isEnable ? "bg-blue-800" : "bg-red-500 !important"
+        }`}
+        onClick={handleClick}
+      >
+        {isEnable ? "Enable" : "Disabled"}
+      </span>
     </>
   );
 };
