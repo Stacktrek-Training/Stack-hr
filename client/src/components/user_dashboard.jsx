60c77ee4dcb5d313498dd6845793998175b3f90f
GilChiu <gilbenedict.chiu-13@cpu.edu.ph>
2023-04-20 13:59:45 +0800

    profile div

--- client/src/components/user_dashboard.jsx
+++ client/src/components/user_dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import profilePicture from "./../assets/profilepic.png";
 
 const User = () => {
   const [showSidebar, setShowSidebar] = useState(false);
@@ -10,7 +11,7 @@ const User = () => {
   return (
     <div>
       {/* Top navbar */}
-      <nav className="bg-white shadow-lg pb-4">
+      <nav className="bg-white shadow-lg pb-4 fixed top-0 w-full z-10">
         <div className="mx-auto px-4">
           <div className="flex justify-between items-center py-4">
             <div className="fixed top-0 left-0 px-4 py-2">
@@ -24,16 +25,16 @@ const User = () => {
       </nav>
 
       {/* Line separator */}
-      <hr className="border-gray-300" />
+      <hr className="border-black fixed" />
 
       {/* Bottom navbar */}
-      <nav className="bg-white shadow-lg pb-3 pt-3">
+      <nav className="bg-white shadow-lg pb-3 pt-3 fixed top-12 left-0 w-full z-10 border-t border-gray-400">
         <div className="mx-auto px-4 flex justify-between items-center">
           {/* Left side: profile picture and username */}
           <div className="flex items-center">
             <img
               className="w-8 h-8 rounded-full mr-2"
-              src="profile-picture.jpg"
+              src={profilePicture}
               alt="Profile"
             />
             <span className="text-gray-700 font-bold">Gil Benedict Chiu</span>
@@ -43,7 +44,7 @@ const User = () => {
           <div className="md:hidden">
             <button
               className="flex items-center px-3 py-2 border rounded text-gray-700 border-gray-700 hover:text-gray-900 hover:border-gray-900"
-              onClick={toggleSidebar} // Add this line to handle the click event
+              onClick={toggleSidebar}
             >
               <svg
                 className="h-4 w-4 mr-1"
@@ -102,6 +103,26 @@ const User = () => {
           </div>
         )}
       </nav>
+      <div className="flex items-center ml-10 mt-10">
+        {/* Left side: profile picture and username */}
+        <div className="w-1/5 mr-4 flex items-center flex-col">
+          <span className="text-gray-700 font-bold mb-2">
+            Gil Benedict Chiu
+          </span>
+          <div className="w-full h-full rounded-lg">
+            <img
+              className="w-full object-cover rounded-lg h-auto max-w-md"
+              src={profilePicture}
+              alt="Profile"
+            />
+          </div>
+        </div>
+
+        {/* Right side: container */}
+        <div className="flex-1 flex items-center">
+          <div className="bg-gray-200 p-4">April</div>
+        </div>
+      </div>
     </div>
   );
 };
