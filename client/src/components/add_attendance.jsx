c41382623c87e5ca436ceaa892e495c4d6b02b1b
16HarryBing <Harrybing.raba@antiquespride.edu.ph>
2023-04-24 09:45:57 +0800

    Modal effect

--- client/src/components/add_attendance.jsx
+++ client/src/components/add_attendance.jsx
@@ -118,7 +118,7 @@ const AddAttendance = () => {
       >
         <div class="relative w-full max-w-md  max-h-full">
           {/* <!-- Modal content --> */}
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 Modal">
             <button
               type="button"
               class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
