666b69d658b09523ef43cb8dc2fc90997133618a
GilChiu <gilbenedict.chiu-13@cpu.edu.ph>
2023-04-28 11:14:13 +0800

    add new button

--- client/src/pages/user_dashboard.jsx
+++ client/src/pages/user_dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./../components/style.css";
 import ExpenseNavbar from "../components/expense_navbar";
 import CircleProgressbar from "../components/user_circle_progressbar";
+import AddNew from "../components/user_addnew";
 
 const User = () => {
   return (
