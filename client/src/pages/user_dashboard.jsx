1ae72aada0e5ea4206a725d0515ffb341625ca5f
GilChiu <gilbenedict.chiu-13@cpu.edu.ph>
2023-05-02 16:04:16 +0800

    user dashboar objects

--- client/src/pages/user_dashboard.jsx
+++ client/src/pages/user_dashboard.jsx
@@ -3,7 +3,7 @@ import "./../components/style.css";
 import ExpenseNavbar from "../components/expense_navbar";
 import CircleProgressbar from "../components/user_circle_progressbar";
 import ButtonAddNew from "../components/user_button_addNew";
-import AddNew from "./user_addnew";
+import profilePicture from "./../assets/profilepic.png";
 
 const User = () => {
   return (
@@ -12,8 +12,26 @@ const User = () => {
       <div>
         <ExpenseNavbar />
       </div>
-      <div>
-        <CircleProgressbar />
+      <div className="flex">
+        <div className=" w-72 h-72  rounded-xl ml-10">
+          <img
+            className="object-cover rounded-lg  "
+            src={profilePicture}
+            alt="Profile"
+          />
+        </div>
+        <div>
+          <div className="flex-1 bg-red-50 mx-10 relative ">
+            <div className=" flex  "></div>
+            {/* Right side: container */}
+            <div className=" Box text-center  flex m-1 md:m-2 bg-red-500">
+              <div class=" ">
+                <CircleProgressbar />
+              </div>{" "}
+              April
+            </div>
+          </div>
+        </div>
       </div>
       <div>
         <ButtonAddNew />
