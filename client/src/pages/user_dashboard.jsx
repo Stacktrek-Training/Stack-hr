4e01f3bdf18e43e805e20fedf6404a74b8e17de6
SunnyVirgo <sunny.virgo@antiquespride.edu.ph>
2023-05-03 13:48:33 +0800

    update

--- client/src/pages/user_dashboard.jsx
+++ client/src/pages/user_dashboard.jsx
@@ -3,23 +3,21 @@ import "./../components/style.css";
 import ExpenseNavbar from "../components/expense_navbar";
 import CircleProgressbar from "../components/user_circle_progressbar";
 import ButtonAddNew from "../components/user_button_addNew";
-import AddNew from "./user_addnew";
 
-const User = () => {
-  return (
+
+const User = () => (
+  <div>
+    <div className="bg-white h-40"></div>
     <div>
-      <div className="bg-white h-40"></div>
-      <div>
-        <ExpenseNavbar />
-      </div>
-      <div>
-        <CircleProgressbar />
-      </div>
-      <div>
-        <ButtonAddNew />
-      </div>
+      <ExpenseNavbar />
     </div>
-  );
-};
+    <div>
+      <CircleProgressbar />
+    </div>
+    <div>
+      <ButtonAddNew />
+    </div>
+  </div>
+);
 
 export default User;
