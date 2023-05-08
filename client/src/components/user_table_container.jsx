e6d57915374dc743f043e1e8ee741e1714a4e522
GilChiu <gilbenedict.chiu-13@cpu.edu.ph>
2023-05-08 23:52:46 +0800

    user page update

--- client/src/components/user_table_container.jsx
+++ client/src/components/user_table_container.jsx
@@ -51,29 +51,23 @@ function TableContainer() {
                 scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                Apple MacBook Pro 17"
+                May 8, 2023
               </th>
-              <td class="px-6 py-4">Silver</td>
-              <td class="px-6 py-4">Laptop</td>
-              <td class="px-6 py-4">$2999</td>
+              <td class="px-6 py-4">Food</td>
+              <td class="px-6 py-4">₱580</td>
               <td class="px-6 py-4">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
+                <button className=" rounded-sm text-blue-400">view</button>{" "}
               </td>
             </tr>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
               <td class="w-4 p-4">
                 <div class="flex items-center">
                   <input
-                    id="checkbox-table-search-2"
+                    id="checkbox-table-search-1"
                     type="checkbox"
                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
-                  <label for="checkbox-table-search-2" class="sr-only">
+                  <label for="checkbox-table-search-1" class="sr-only">
                     checkbox
                   </label>
                 </div>
@@ -82,29 +76,23 @@ function TableContainer() {
                 scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                Microsoft Surface Pro
+                May 7, 2023
               </th>
-              <td class="px-6 py-4">White</td>
-              <td class="px-6 py-4">Laptop PC</td>
-              <td class="px-6 py-4">$1999</td>
+              <td class="px-6 py-4">Transportation</td>
+              <td class="px-6 py-4">₱700</td>
               <td class="px-6 py-4">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
+                <button className=" rounded-sm text-blue-400">view</button>{" "}
               </td>
             </tr>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
               <td class="w-4 p-4">
                 <div class="flex items-center">
                   <input
-                    id="checkbox-table-search-3"
+                    id="checkbox-table-search-1"
                     type="checkbox"
                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
-                  <label for="checkbox-table-search-3" class="sr-only">
+                  <label for="checkbox-table-search-1" class="sr-only">
                     checkbox
                   </label>
                 </div>
@@ -113,29 +101,23 @@ function TableContainer() {
                 scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                Magic Mouse 2
+                May 5, 2023
               </th>
-              <td class="px-6 py-4">Black</td>
-              <td class="px-6 py-4">Accessories</td>
-              <td class="px-6 py-4">$99</td>
+              <td class="px-6 py-4">Food</td>
+              <td class="px-6 py-4">₱300</td>
               <td class="px-6 py-4">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
+                <button className=" rounded-sm text-blue-400">view</button>{" "}
               </td>
             </tr>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
               <td class="w-4 p-4">
                 <div class="flex items-center">
                   <input
-                    id="checkbox-table-3"
+                    id="checkbox-table-search-1"
                     type="checkbox"
                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
-                  <label for="checkbox-table-3" class="sr-only">
+                  <label for="checkbox-table-search-1" class="sr-only">
                     checkbox
                   </label>
                 </div>
@@ -144,29 +126,23 @@ function TableContainer() {
                 scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                Apple Watch
+                May 3, 2023
               </th>
-              <td class="px-6 py-4">Silver</td>
-              <td class="px-6 py-4">Accessories</td>
-              <td class="px-6 py-4">$179</td>
+              <td class="px-6 py-4">Internet</td>
+              <td class="px-6 py-4">₱1500</td>
               <td class="px-6 py-4">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
+                <button className=" rounded-sm text-blue-400">view</button>{" "}
               </td>
             </tr>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
               <td class="w-4 p-4">
                 <div class="flex items-center">
                   <input
-                    id="checkbox-table-3"
+                    id="checkbox-table-search-1"
                     type="checkbox"
                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
-                  <label for="checkbox-table-3" class="sr-only">
+                  <label for="checkbox-table-search-1" class="sr-only">
                     checkbox
                   </label>
                 </div>
@@ -175,29 +151,23 @@ function TableContainer() {
                 scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                iPad
+                May 2, 2023
               </th>
-              <td class="px-6 py-4">Gold</td>
-              <td class="px-6 py-4">Tablet</td>
-              <td class="px-6 py-4">$699</td>
+              <td class="px-6 py-4">Non-Coding Activity</td>
+              <td class="px-6 py-4">₱500</td>
               <td class="px-6 py-4">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
+                <button className=" rounded-sm text-blue-400">view</button>{" "}
               </td>
             </tr>
-            <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
               <td class="w-4 p-4">
                 <div class="flex items-center">
                   <input
-                    id="checkbox-table-3"
+                    id="checkbox-table-search-1"
                     type="checkbox"
                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
-                  <label for="checkbox-table-3" class="sr-only">
+                  <label for="checkbox-table-search-1" class="sr-only">
                     checkbox
                   </label>
                 </div>
@@ -206,18 +176,12 @@ function TableContainer() {
                 scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                Apple iMac 27"
+                May 1, 2023
               </th>
-              <td class="px-6 py-4">Silver</td>
-              <td class="px-6 py-4">PC Desktop</td>
-              <td class="px-6 py-4">$3999</td>
+              <td class="px-6 py-4">Office Supplies</td>
+              <td class="px-6 py-4">₱400</td>
               <td class="px-6 py-4">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
+                <button className=" rounded-sm text-blue-400">view</button>{" "}
               </td>
             </tr>
           </tbody>
