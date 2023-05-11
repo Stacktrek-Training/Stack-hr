7634f8fd09571a76dc0795edfd7c37bb6c15ca5f
GilChiu <gilbenedict.chiu-13@cpu.edu.ph>
2023-05-11 17:22:16 +0800

    added hover animation for 1st most spent in reimbursement

--- client/src/components/user_container_dashboard.jsx
+++ client/src/components/user_container_dashboard.jsx
@@ -27,7 +27,9 @@ function ContainerDashboard() {
               {/* Top 3 */}
               <div className="p-10 mt-5">
                 <div className="flex items-center">
-                  <span className="mr-4 text-left">1. Food</span>
+                  <span className="mr-4 text-left transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                    1. Food
+                  </span>
                   <div className="w-1/2 h-4 bg-gray-300 rounded-full overflow-hidden">
                     <div className="h-full bg-blue-500 rounded-full animate-pulse"></div>
                   </div>
