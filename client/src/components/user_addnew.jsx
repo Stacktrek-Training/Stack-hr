eb82a31a9696651bd445d151fca801f0293b19de
GilChiu <gilbenedict.chiu-13@cpu.edu.ph>
2023-05-12 22:13:07 +0800

    Updated add new button for user dashboard for better readability

--- client/src/components/user_addnew.jsx
+++ client/src/components/user_addnew.jsx
@@ -56,65 +56,62 @@ function AddNew({ visible, onClose }) {
             />
           </svg>
         </button>
-        <form onSubmit={handleSave}>
-          <div className="bg-white p-2 rounded">
-            <div className="flex flex-col md:flex-row md:items-center">
-              {/* Date picker */}
-              <div className="w-full md:w-auto mb-2 md:mb-0 md:mr-2 mt-5">
-                <label class="block mb-2 font-bold">
-                  Available Funds: <span class="text-orange-500">₱8900</span>
-                </label>
+        <form onSubmit={handleSave} className="bg-white p-2 rounded">
+          <div className="flex flex-col md:flex-row md:items-center">
+            {/* Date picker */}
+            <div className="w-full md:w-auto mb-2 md:mb-0 md:mr-2 mt-5">
+              <label class="block mb-2 font-bold">
+                Available Funds: <span class="text-orange-500">₱8900</span>
+              </label>
 
-                <label class="block mb-2 font-bold" for="date">
-                  Choose a date:
-                </label>
-                <input
-                  type="date"
-                  className="p-2 rounded border-gray-300 w-full"
-                  value={date}
-                  onChange={(e) => setDate(e.target.value)}
-                />
-              </div>
-
-              {/* Dropdown */}
-              <div className="w-full md:w-auto mb-2 md:mb-0 md:mx-2">
-                <label class="block mb-2 font-bold" for="date">
-                  Choose a Category:
-                </label>
-                <select
-                  className="p-2 rounded border-gray-300 w-full"
-                  value={category}
-                  onChange={(e) => setCategory(e.target.value)}
-                >
-                  <option value="Transportation">Transportation</option>
-                  <option value="Other Expenses">Other Expenses</option>
-                  <option value="Non-Coding Activity">
-                    Non-Coding Activity
-                  </option>
-                  <option value="Internet">Internet</option>
-                  <option value=">Office Supplies">Office Supplies</option>
-                  <option value="Meals">Meals</option>
-                  <option value="Medicine">Medicine</option>
-                </select>
-              </div>
-
-              {/* Amount input */}
-              <div className="w-full md:w-auto mb-2 md:mb-0 md:mx-2 relative">
-                <label class="block mb-2 font-bold" for="date">
-                  Enter Amount:
-                </label>
-                <input
-                  type="number"
-                  className="p-2 rounded border-gray-300 w-full"
-                  placeholder="Enter amount"
-                  value={amount}
-                  onChange={(e) => setamount(e.target.value)}
-                />
-              </div>
+              <label class="block mb-2 font-bold" for="date">
+                Choose a date:
+              </label>
+              <input
+                type="date"
+                className="p-2 rounded border-gray-300 w-full"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
             </div>
 
-            {/* Upload image */}
+            {/* Dropdown */}
+            <div className="w-full md:w-auto mb-2 md:mb-0 md:mx-2">
+              <label class="block mb-2 font-bold" for="date">
+                Choose a Category:
+              </label>
+              <select
+                className="p-2 rounded border-gray-300 w-full"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="Transportation">Transportation</option>
+                <option value="Other Expenses">Other Expenses</option>
+                <option value="Non-Coding Activity">Non-Coding Activity</option>
+                <option value="Internet">Internet</option>
+                <option value="Office Supplies">Office Supplies</option>
+                <option value="Meals">Meals</option>
+                <option value="Medicine">Medicine</option>
+              </select>
+            </div>
 
+            {/* Amount input */}
+            <div className="w-full md:w-auto mb-2 md:mb-0 md:mx-2 relative">
+              <label class="block mb-2 font-bold" for="date">
+                Enter Amount:
+              </label>
+              <input
+                type="number"
+                className="p-2 rounded border-gray-300 w-full"
+                placeholder="Enter amount"
+                value={amount}
+                onChange={(e) => setamount(e.target.value)}
+              />
+            </div>
+          </div>
+
+          {/* Upload image */}
+          <div className="mt-5">
             <label class="block mb-2 font-bold" for="date">
               Upload Receipt
             </label>
@@ -123,16 +120,16 @@ function AddNew({ visible, onClose }) {
               id="file_input"
               type="file"
             />
+          </div>
 
-            {/* Add new button */}
-            <div className="flex justify-end m-1 md:m-2">
-              <button
-                type="submit"
-                class="bg-gradient-to-br from-orange-400 via-f0b673 to-orange-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-orange-200 dark:focus:ring-orange-800 text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-              >
-                Add New
-              </button>
-            </div>
+          {/* Add new button */}
+          <div className="flex justify-end mt-5">
+            <button
+              type="submit"
+              class="bg-gradient-to-br from-orange-400 via-f0b673 to-orange-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-orange-200 dark:focus:ring-orange-800 text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            >
+              Add New
+            </button>
           </div>
         </form>
       </div>
