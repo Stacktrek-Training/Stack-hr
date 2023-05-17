f431e979395da0f3c2a7bda94c4baea0921bbec0
GilChiu <gilbenedict.chiu-13@cpu.edu.ph>
2023-05-17 12:01:12 +0800

    added a my-div class that will show only on small screens and added to top 3 most spent categories on user dashboard

--- client/src/components/user_table_container.jsx
+++ client/src/components/user_table_container.jsx
@@ -19,6 +19,7 @@ function TableContainer() {
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead class="text-xs text-gray-700 uppercase bg-gradient-to-br from-orange-500 to-orange-400 dark:bg-gray-700 dark:text-gray-400">
             <tr>
+              <th scope="col" class="p-4"></th>
               <th scope="col" class="px-6 py-3 text-white">
                 Date
               </th>
