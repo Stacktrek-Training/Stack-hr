0aec54fdfd3e918e99eaa60c022744e17e76609c
GilChiu <gilbenedict.chiu-13@cpu.edu.ph>
2023-05-18 11:31:20 +0800

    added useEffect on add new modal to get reimbursed limit and total amount of reimbursements and displayed available funds by subtracting total amount to limit

--- client/src/components/user_addnew.jsx
+++ client/src/components/user_addnew.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./../components/style.css";
 import axios from "axios";
 
@@ -13,6 +13,35 @@ function AddNew({ visible, onClose, employee }) {
   const [category, setCategory] = useState("");
   const [amount, setamount] = useState("");
   const [receipt, setReceipt] = useState("");
+
+  const [reimburseLimit, setReimburseLimit] = useState(null);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    const fetchEmployee = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/employee/${employee}`
+        );
+        setReimburseLimit(response.data[0].reimbursed_limit);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchEmployee();
+  }, [employee]);
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth() + 1; // add 1 to convert to 1-based index
+    axios
+      .get(`http://localhost:4000/sum/${employee}/${currentMonth}`)
+      .then((response) => {
+        setTotalAmount(response.data.total_amount);
+      })
+      .catch((error) => console.error(error));
+  }, [employee]);
 
   const handleSave = () => {
     axios
@@ -30,7 +59,7 @@ function AddNew({ visible, onClose, employee }) {
         console.error(error.message);
       });
   };
-
+  const availableFunds = reimburseLimit - totalAmount;
   return (
     <div
       id="container"
@@ -63,7 +92,8 @@ function AddNew({ visible, onClose, employee }) {
             {/* Date picker */}
             <div className="w-full md:w-auto mb-2 md:mb-0 md:mr-2 mt-5">
               <label class="block mb-2 font-bold">
-                Available Funds: <span class="text-orange-500">₱8900</span>
+                Available Funds:{" "}
+                <span class="text-orange-500">₱{availableFunds}</span>
               </label>
 
               <label class="block mb-2 font-bold" for="date">
