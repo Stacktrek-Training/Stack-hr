45c7cafd72696c13ceec10e886ce1172650d9182
GilChiu <gilbenedict.chiu-13@cpu.edu.ph>
2023-05-18 11:13:02 +0800

    passed employee_id to user container dashboard and circleprogress bar to display specific employee profile data

--- client/src/components/user_edit_limit_button.jsx
+++ client/src/components/user_edit_limit_button.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import EditLimitModal from "./user_edit_limit_modal";
 
-function EditLimitButton() {
+function EditLimitButton({ employee }) {
   const [showEditLimitModal, setshowEditLimitModal] = useState(false);
 
   const handleOnClose = () => setshowEditLimitModal(false);
@@ -10,12 +10,17 @@ function EditLimitButton() {
       <button
         type="button"
         onClick={() => setshowEditLimitModal(true)}
-        class="absolute bottom-2 right-2 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+        class="absolute bottom-2 right-2 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
         Edit Limit
       </button>
 
       <div>
-        <EditLimitModal onClose={handleOnClose} visible={showEditLimitModal} />
+        <EditLimitModal
+          employee={employee}
+          onClose={handleOnClose}
+          visible={showEditLimitModal}
+        />
       </div>
     </div>
   );
