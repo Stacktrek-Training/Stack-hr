7c14e8bb2f632855bb6707734bed2ebff602ac64
GilChiu <gilbenedict.chiu-13@cpu.edu.ph>
2023-05-19 10:22:15 +0800

    fixed issue with add new not adding employee id

--- client/src/progressbar.js
+++ client/src/progressbar.js
@@ -1,10 +1,12 @@
-let number = document.getElementById("number");
-let counter = 0;
-setInterval(() => {
-  if (counter == 65) {
-    clearInterval();
-  } else {
-    counter += 1;
-    number.innerHTML = counter + "%";
-  }
-}, 30);
+export function updateCounter(percent) {
+  let number = document.getElementById("percentnumber");
+  let counter = 0;
+  setInterval(() => {
+    if (counter == percent) {
+      clearInterval();
+    } else {
+      counter += 1;
+      number.innerHTML = counter + "%";
+    }
+  }, 30);
+}
